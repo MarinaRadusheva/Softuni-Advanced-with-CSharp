a259531c810559e2706c4c955e456b11d11e6349
Marina <marinaradusheva9@gmail.com>
2020-11-30 17:19:00 +0200

    adding unit testing files

---
+++ OOP/OOP 08 Unit Testing Lab/Tests/AxeTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        [Test]
+        public void AxeShouldLoseDurabilityAfterAttack()
+        {
+            // Arrange
+            Axe axe = new Axe(15, 20);
+            Dummy dummy = new Dummy(30, 30);
+            // Act
+            axe.Attack(dummy);
+            // Assert
+            int expectedResult = 19;
+            int actualResult = axe.DurabilityPoints;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void AttackWithABrokenWeaponShouldThrowException()
+        {
+            // Arrange
+            Axe axe = new Axe(10, 0);
+            Dummy dummy = new Dummy(10, 20);
+            // Act - Assert
+            Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+        }
+    }
+}
---
+++ OOP/OOP 08 Unit Testing Lab/Tests/DummyTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        private Axe axe = new Axe(20, 20);
+        private Dummy dummy;
+        [Test]
+        [TestCase(50, 20)]
+        public void DummyShouldLoseHealthIfAttacked(int health, int exp)
+        {
+            //Arrange
+            dummy = new Dummy(health, exp);
+            // Act
+            axe.Attack(dummy);
+            // Assert
+            bool expectedResult = true;
+            bool actualResult = dummy.Health < health;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void DeadDummyShouldThrowsExceptionIfAttacked()
+        {
+            // Arrange
+            dummy = new Dummy(0, 20);
+            //Act-Assert
+            Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+        }
+        [Test]
+        [TestCase(0, 20)]
+        public void DeadDummyShouldGiveXP(int health, int experience)
+        {
+            // Arrange
+            dummy = new Dummy(0, 20);
+            // Act
+            int exp = dummy.GiveExperience();
+            // Assert
+            Assert.That(exp.Equals(experience));
+        }
+        [Test]
+        public void AliveDummyShouldNotBeAbletoGiveXP()
+        {
+            //Arrange
+            dummy = new Dummy(20, 20);
+            //Act-Assert
+            Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+        }
+
+
+    }
+}
