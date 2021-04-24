3af87aec4555e2edbabf4893df8569ba586e1d65
Marina <marinaradusheva9@gmail.com>
2021-04-24 10:25:15 +0300

    custom stack impleentation

--- Advanced/Advanced 07 Custom Data Structures Implementation/Custom Stack Implementation/CustomStack.cs
+++ Advanced/Advanced 07 Custom Data Structures Implementation/Custom Stack Implementation/CustomStack.cs
@@ -4,7 +4,63 @@ using System.Text;
 
 namespace Custom_Stack_Implementation
 {
-    class CustomStack
+    public class CustomStack
     {
+        private const int InitialCapacity = 4;
+        private int[] items;
+        private int count;
+        public CustomStack()
+        {
+            this.count = 0;
+            this.items = new int[InitialCapacity];
+        }
+        public int Count { get => this.count; }
+        public void Push(int element)
+        {
+            if (this.count==this.items.Length)
+            {
+                Resize();
+            }
+            this.items[count] = element;
+            count++;
+        }
+        public int Pop()
+        {
+            if (items.Length==0)
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+            int lastIndex = this.count - 1;
+            int removedItem = this.items[lastIndex];
+            this.items[lastIndex] = default(int);
+            count--;
+            return removedItem;
+        }
+
+        public int Peek()
+        {
+            if (items.Length == 0)
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+            int item = this.items[count - 1];
+            return item;
+        }
+        public void ForEach(Action<int> action)
+        {
+            for (int i = 0; i < this.count; i++)
+            {
+                action(this.items[i]);
+            }
+        }
+        private void Resize()
+        {
+            int[] copy = new int[items.Length * 2];
+            for (int i = 0; i < this.count; i++)
+            {
+                copy[i] = items[i];
+            }
+            items = copy;
+        }
     }
 }
--- Advanced/Advanced 07 Custom Data Structures Implementation/Custom Stack Implementation/Program.cs
+++ Advanced/Advanced 07 Custom Data Structures Implementation/Custom Stack Implementation/Program.cs
@@ -6,7 +6,18 @@ namespace Custom_Stack_Implementation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CustomStack stack = new CustomStack();
+            stack.Push(5);
+            stack.Push(7);
+            stack.Push(2);
+            stack.Push(4);
+            stack.Push(7);
+            stack.Push(6);
+            stack.Push(3);
+            stack.Push(1);
+            stack.Pop();
+            Console.WriteLine(stack.Peek());
+            stack.ForEach(x=>Console.WriteLine(x));
         }
     }
 }
